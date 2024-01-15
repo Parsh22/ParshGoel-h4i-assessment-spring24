2bfc4b41b6049417d9873b078221e100c6f65472
Parsh22 <70302713+Parsh22@users.noreply.github.com>
2024-01-15 11:47:03 -0500

    artist page implemented

--- assessment/src/artistPage.jsx
+++ assessment/src/artistPage.jsx
@@ -1,12 +1,34 @@
-import './artistPage.css'
+import React from "react";
 
-/*
-This component is intended to create a page for each artist.
-You can use this component if you want, but it is not required.
-*/
+import "./artistPage.css";
+import "./artistPage.css";
 
-const ArtistPage = (props) => {
-   
-}
+const ArtistPage = ({ artist }) => {
+  return (
+    <div className="artist-container">
+      <div className="artist-card">
+        <h2 className="artist-name">{artist.name}</h2>
+        <p className="artist-minutes">
+          Minutes spent listening: {artist.minutes}
+        </p>
+        <div className="artist-image-div">
+          <img src={artist.image_src} className="artist-image"></img>
+        </div>
+        <div className="artist-top-songs">
+          <h3>Top Songs:</h3>
+          <ul>
+            {artist.songs.map((song, index) => (
+              <li key={index}>{song}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {/* <div className="navigation">
+        <div className="nav-arrow left-arrow">←</div>
+        <div className="nav-arrow right-arrow">→</div>
+      </div> */}
+    </div>
+  );
+};
 
-export default ArtistPage
+export default ArtistPage;
--- assessment/src/spotify.jsx
+++ assessment/src/spotify.jsx
@@ -1,13 +1,41 @@
-import artists from './artists.jsx'
-import './spotify.css'
+import React, { useState } from "react";
+
+import artists from "./artists.jsx";
+import "./spotify.css";
+import ArtistPage from "./artistPage";
+
 /* 
 This is the component for the entire page. You can make additional components as you please.
 Feel free to edit anything in this file except the name of the component.
 */
 const Spotify = (props) => {
-    return (
-        <div>Spotify</div>
-    )
-}
+  const [currArtist, setCurrArtist] = useState(0);
 
-export default Spotify
+  const handleNextArtist = () => {
+    setCurrArtist((prevArtist) => (prevArtist + 1) % artists.length);
+  };
+
+  const handlePrevArtist = () => {
+    setCurrArtist(
+      (prevArtist) => (prevArtist - 1 + artists.length) % artists.length
+    );
+  };
+
+  const curr = artists[currArtist];
+
+  return (
+    <div className="spotify-app">
+      <div className="nav-arrows">
+        <button className="nav-arrow left-arrow" onClick={handlePrevArtist}>
+          &#8249;
+        </button>
+        <button className="nav-arrow right-arrow" onClick={handleNextArtist}>
+          &#8250;
+        </button>
+      </div>
+      <ArtistPage artist={curr} />
+    </div>
+  );
+};
+
+export default Spotify;
